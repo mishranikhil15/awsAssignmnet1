73dad6da4477989fd79db9b76112fd960dd23f35
Nikhil Kumar <vashishtnikhil158@gmail.com>
2023-03-19 16:27:26 +0530

    aws2

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("rdsTest", "admin", "abcd1234", {
+const sequelize = new Sequelize("rdsTest", "root", "Nikhil123", {
   host: "database-1.cnpzdb4g9bfs.eu-north-1.rds.amazonaws.com",
   dialect: "mysql",
 });
